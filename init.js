c409aef5dd21c288ca864a030e108f3a87bd490f
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-08-16 20:41:02 +0200

    feat(Meteor0.8.3): Fix check

--- init.js
+++ init.js
@@ -11,7 +11,7 @@ UiRouter = {
     template: function(templName) {
         return function() {
             // test for pre 0.8.3
-            if (Template[templName].render())
+            if (Template[templName].render)
               return Template[templName].render().toHTML();
             else
               return Blaze.toHTML(Template[templName]);
